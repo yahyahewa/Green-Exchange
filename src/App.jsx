961dc1a62ef43f4aafd4653631c2b3c470caf208
rageofkurd <rageofkurd98@gmail.com>
2023-06-23 02:49:20 +0300

    browser router been configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
-
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-4xl font-bold'>React App</h1>
+      <Routes>
+      <Route path="/" element={<HomePage/>} />
+      </Routes>
     </>
   )
 }
