86be30c3fe94cdfad5af65c9e6894aa5e8e42a17
yahyahiwa3 <yahyahiwa3@gmail.com>
2023-06-23 16:36:48 +0300

    done with navbar design

--- src/components/navbar/DropDownMenu.jsx
+++ src/components/navbar/DropDownMenu.jsx
@@ -1,20 +1,18 @@
-
-import NavItem from './NavItem';
-import { useDisclosure } from '@mantine/hooks';
-import { Drawer, Group } from '@mantine/core';
-import Humberger from './Humberger';
+import NavItem from "./NavItem";
+import { useDisclosure } from "@mantine/hooks";
+// import { useDisclosure } from '@mantine/hooks';
+import { Drawer, Group } from "@mantine/core";
+import Humberger from "./Humberger";
 
 const LINKS = [
-    { title: 'Home', path: '/' },
-    { title: 'Items', path: '/Items' },
-    { title: 'About', path: '/foundItems' },
-    { title: 'F&Q', path: '/faq' },
-    
-  ];
+  { title: "Home", path: "/" },
+  { title: "Items", path: "/Items" },
+  { title: "About", path: "/foundItems" },
+  { title: "F&Q", path: "/faq" },
+];
 
 function DropDownMenu() {
   const [opened, { open, close }] = useDisclosure(false);
-
 
   return (
     <>
@@ -24,20 +22,19 @@ function DropDownMenu() {
         onClose={close}
         withCloseButton={false}
         overlayProps={{ opacity: 0.85 }}
-        size="80%">
+        size="80%"
+      >
         <Drawer.Header>
           <Drawer.Title className=" w-full text-center"></Drawer.Title>
-          <Drawer.CloseButton size={'md'} />
+          <Drawer.CloseButton size={"md"} />
         </Drawer.Header>
         <Drawer.Body className="overflow-hidden">
           <ul className="flex flex-col justify-center items-center gap-10 text-xl text-center   bg-white hover:text-white">
-           
             {LINKS.map((link, index) => (
               <NavItem key={index} href={link.path} toggleDropDown={close}>
                 {link.title}
               </NavItem>
             ))}
-           
           </ul>
         </Drawer.Body>
         {/* Drawer content */}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,24 @@
-
 import { Link } from "react-router-dom";
 import NavItem from "./NavItem";
 import Logo from "./Logo";
 import DropDownMenu from "./DropDownMenu";
 
-
 const LINKS = [
-  { title: 'Home', path: '/' },
-  { title: 'Items', path: '/Items' },
-  { title: 'About', path: '/foundItems' },
-  { title: 'F&Q', path: '/faq' },
-  
+  { title: "Home", path: "/" },
+  { title: "Items", path: "/Items" },
+  { title: "About", path: "/foundItems" },
+  { title: "F&Q", path: "/faq" },
 ];
 
 const NavbarComponent = ({ toggleDropDown }) => {
-  /
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light flex justify-between items-center px-10 py-4 lg:max-w-[85rem] mx-auto ">
       <Link to="/" className="font-bold  text-md text-neutral-600">
-        <Logo/>
+        <Logo />
       </Link>
 
       <div className="md:hidden" onClick={toggleDropDown}>
-       <DropDownMenu/>
+        <DropDownMenu />
       </div>
 
       <div className=" hidden md:block">
@@ -34,8 +30,9 @@ const NavbarComponent = ({ toggleDropDown }) => {
               </NavItem>
             ))}
           </ul>
-          <button className='btn btn-primary py-1  rounded-md btn-md bg-green-700/80 text-neutral-100 border-0  hover:bg-green-700 transition-all'>Get Started</button>
-        
+          <button className="btn btn-primary py-1  rounded-md btn-md bg-green-700/80 text-neutral-100 border-0  hover:bg-green-700 transition-all">
+            Get Started
+          </button>
         </div>
       </div>
     </nav>
