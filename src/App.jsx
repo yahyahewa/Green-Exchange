fe2ad34bddaac6e37a3d605f8cf3317e85396092
rozh bahman <r.bahman98@gmail.com>
2023-06-23 14:57:52 +0300

    Add the signup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route exact path="/login" element={<Login />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
