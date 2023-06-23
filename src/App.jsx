a2b0d020e3c636307af56095dcf0e20f424d691f
rozh bahman <r.bahman98@gmail.com>
2023-06-23 14:11:52 +0300

    Create login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-
-
   return (
     <>
       <Routes>
-      <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
